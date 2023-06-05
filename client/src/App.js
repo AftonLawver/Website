65407ded815d1af29cbc0eecd93705d0a3b2f2d1
Afton Lawver <lawverap25@gmail.com>
2023-06-05 09:20:07 -0500

    Deleted some info from website (pics, videos, music) to make more professional and less cluttered. Began working on another version of my blog. Still debating on actually implementing new version.

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+  return (
+      <main>
+        <header>
+          <a href="" className="logo">MyBlog</a>
+            <nav>
+                <a href="">Login</a>
+                <a href="">Register</a>
+            </nav>
+        </header>
+          <div className="post">
+              <img src="https://media.gcflearnfree.org/content/5e31ca08bc7eff08e4063776_01_29_2020/ProgrammingIllustration.png" alt="photo not found"></img>
+              <h2>Java Methods</h2>
+              <p>Java is an object oriented programming language where
+                  all source code must be written within a class. In these
+                  classes, you will often write methods that do specific tasks.
+                  Every method has a specific syntax that must be followed for
+                  the program to be compiled correctly. This syntax is what will
+                  tell the programmer the scope, the return type, the name,
+                  and the specific parameters
+                  that need to be passed into the method for it to function correctly.
+              </p>
+          </div>
+          <div className="post">
+              <img src="https://media.gcflearnfree.org/content/5e31ca08bc7eff08e4063776_01_29_2020/ProgrammingIllustration.png" alt="photo not found"></img>
+              <h2>Java Methods</h2>
+              <p>Java is an object oriented programming language where
+                  all source code must be written within a class. In these
+                  classes, you will often write methods that do specific tasks.
+                  Every method has a specific syntax that must be followed for
+                  the program to be compiled correctly. This syntax is what will
+                  tell the programmer the scope, the return type, the name,
+                  and the specific parameters
+                  that need to be passed into the method for it to function correctly.
+              </p>
+          </div>
+          <div className="post">
+              <img src="https://media.gcflearnfree.org/content/5e31ca08bc7eff08e4063776_01_29_2020/ProgrammingIllustration.png" alt="photo not found"></img>
+              <h2>Java Methods</h2>
+              <p>Java is an object oriented programming language where
+                  all source code must be written within a class. In these
+                  classes, you will often write methods that do specific tasks.
+                  Every method has a specific syntax that must be followed for
+                  the program to be compiled correctly. This syntax is what will
+                  tell the programmer the scope, the return type, the name,
+                  and the specific parameters
+                  that need to be passed into the method for it to function correctly.
+              </p>
+          </div>
+      </main>
+  );
+}
+
+export default App;
